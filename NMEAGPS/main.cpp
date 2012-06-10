6af9bdfbe8bfeefc5fc8cb7894b4ad11c3d9fbdd
Rowland O'Flaherty <rowoflo@gmail.com>
2012-06-10 17:40:52 -0400

    Updated to include gps.valid() function.

--- NMEAGPS/main.cpp
+++ NMEAGPS/main.cpp
@@ -16,6 +16,7 @@
 //------------------------------------------------------------------------------
 #include <Arduino.h>
 #include <NMEAGPS.h>
+#include <SoftwareSerial.h>
 
 //------------------------------------------------------------------------------
 // Helper Function Declarations
@@ -27,11 +28,14 @@
 //------------------------------------------------------------------------------
 NMEAGPS gps(2,3);
 
+SoftwareSerial ss(4,5);
+
 //------------------------------------------------------------------------------
 // Setup Function - Initializes Arduino
 //------------------------------------------------------------------------------
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
+    ss.begin(9600);
     gps.initialize();
 }
 
@@ -39,47 +43,61 @@ void setup() {
 // Loop Function - Arduino Program Loop
 //------------------------------------------------------------------------------
 void loop() {
-    gps.update();
+    gps.listen();
+    delay(250);
     
     Serial.print("Overflow: ");
     Serial.println(gps.overflow());
     
-    Serial.print("Time: ");
-    Serial.print(gps.hour()); Serial.print(":");
-    Serial.print(gps.minute()); Serial.print(":");
-    Serial.println(gps.second());
+    if (gps.update() && gps.valid()) {
+        
+        Serial.print("Valid: ");
+        Serial.println(gps.valid());
+        
+        Serial.print("Time: ");
+        Serial.print(gps.hour()); Serial.print(":");
+        Serial.print(gps.minute()); Serial.print(":");
+        Serial.println(gps.second());
 
-    Serial.print("Lat: ");
-    Serial.println(gps.latitude(),6);
+        Serial.print("Lat: ");
+        Serial.println(gps.latitude(),6);
 
-    Serial.print("Lon: ");
-    Serial.println(gps.longitude(),6);
+        Serial.print("Lon: ");
+        Serial.println(gps.longitude(),6);
+        
+        Serial.print("Alt: ");
+        Serial.print(gps.altitude()); Serial.print(" "); Serial.println(gps.altUnits());
+        
+        Serial.print("Geoid: ");
+        Serial.print(gps.geoid()); Serial.print(" "); Serial.println(gps.geoidUnits());
+        
+        Serial.print("Speed: ");
+        Serial.println(gps.speed());
+        
+        Serial.print("Course: ");
+        Serial.println(gps.course());
+
+        Serial.print("# Sats: ");
+        Serial.println(gps.numberOfSats());
+
+        Serial.print("PDOP: ");
+        Serial.println(gps.PDOP());
+        
+        Serial.print("HDOP: ");
+        Serial.println(gps.HDOP());    
+
+        Serial.print("VDOP: ");
+        Serial.println(gps.VDOP());
+        
+        Serial.println();
+        
+    } else {
+        Serial.println("No new valid GPS data ...");
+    }
     
-    Serial.print("Alt: ");
-    Serial.print(gps.altitude()); Serial.print(" "); Serial.println(gps.altUnits());
+    ss.listen();
+    delay(5);
     
-    Serial.print("Geoid: ");
-    Serial.print(gps.geoid()); Serial.print(" "); Serial.println(gps.geoidUnits());
-    
-    Serial.print("Speed: ");
-    Serial.println(gps.speed());
-    
-    Serial.print("Course: ");
-    Serial.println(gps.course());
-
-    Serial.print("# Sats: ");
-    Serial.println(gps.numberOfSats());
-
-    Serial.print("PDOP: ");
-    Serial.println(gps.PDOP());
-    
-    Serial.print("HDOP: ");
-    Serial.println(gps.HDOP());    
-
-    Serial.print("VDOP: ");
-    Serial.println(gps.VDOP());
-
-    delay(500);
 }
 
 //------------------------------------------------------------------------------
